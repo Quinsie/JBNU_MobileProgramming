5088ac0dc00d0c561f0d385070ac981e22fed04f
Quinsie <jyjh0501@naver.com>
2025-04-21 12:37:54 +0900

    abspath and call

--- backend/source/scripts/weatherCollecter.py
+++ backend/source/scripts/weatherCollecter.py
@@ -8,7 +8,7 @@ import requests
 from datetime import datetime, timedelta
 
 # 상대 경로 import 설정
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..")); sys.path.append(BASE_DIR)
 from utils.haversine import haversine_distance  # 거리 계산용
 from utils.logger import log  # log 함수 추가
 
