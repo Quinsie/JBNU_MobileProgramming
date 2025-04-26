daf6841a47a90f4b2fd4ff3ca0eaa03bed252cbe
Quinsie <jyjh0501@naver.com>
2025-04-27 08:07:57 +0900

    modified generateParquet :: restart modeling

--- backend/source/ai/generateParquet.py
+++ backend/source/ai/generateParquet.py
@@ -15,7 +15,7 @@ from source.utils.getDayType import getDayType
 def main():
     # 경로 설정
     ETA_TABLE_DIR = os.path.join(BASE_DIR, 'data', 'preprocessed', 'eta_table')
-    REALTIME_BUS_DIR = os.path.join(BASE_DIR, 'data', 'dynamicInfo', 'realtime_bus')
+    REALTIME_BUS_DIR = os.path.join(BASE_DIR, 'data', 'raw', 'dynamicInfo', 'realtime_bus')
     WEATHER_DIR = os.path.join(BASE_DIR, 'data', 'raw', 'dynamicInfo', 'weather')
     STDID_TO_STOPS_PATH = os.path.join(BASE_DIR, 'data', 'processed', 'stdid_to_stops.json')
     STDID_NUMBER_PATH = os.path.join(BASE_DIR, 'data', 'processed', 'stdid_number.json')
