0d50fc8c8c6d3cd8fd1633c87896ff8997c0110c
Quinsie <jyjh0501@naver.com>
2025-05-02 19:08:13 +0900

    modified day :: experiment - data

--- backend/source/ai/generateFirstETATable_1.py
+++ backend/source/ai/generateFirstETATable_1.py
@@ -16,7 +16,7 @@ sys.path.append(BASE_DIR)
 
 # 날짜 설정
 # TODAY = datetime.now()
-TODAY = datetime(2025, 4, 30)  # 지금 날짜
+TODAY = datetime(2025, 5, 1)  # 지금 날짜
 YESTERDAY_DATE = TODAY - timedelta(days=1)  # 학습용 어제 날짜
 TARGET_DATE = TODAY  # 추론 목표 날짜
 
