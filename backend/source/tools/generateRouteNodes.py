81a8ff5bc0e869e441f12676d1b24028df959675
Quinsie <jyjh0501@naver.com>
2025-05-04 16:57:24 +0900

    modified generateRouteNodes.py

--- backend/source/tools/generateRouteNodes.py
+++ backend/source/tools/generateRouteNodes.py
@@ -14,7 +14,7 @@ STOP_DIR = os.path.join(BASE_DIR, "data", "raw", "staticInfo", "stops")
 VTX_DIR = os.path.join(BASE_DIR, "data", "raw", "staticInfo", "vtx")
 SAVE_DIR = os.path.join(BASE_DIR, "data", "processed", "route_nodes")
 
-STOP_MATCH_THRESHOLD = 100  # 범위 확장
+STOP_MATCH_THRESHOLD = 30
 SAMPLE_INTERVAL = 200
 FINE_STEP = 1
 
