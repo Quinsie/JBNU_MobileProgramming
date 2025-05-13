a127f6a9c21798d85ec15300b9ba59faa0a632b3
Quinsie <jyjh0501@naver.com>
2025-05-13 20:49:59 +0900

    0510train

--- backend/source/ai/generateFirstETATable.py
+++ backend/source/ai/generateFirstETATable.py
@@ -19,7 +19,7 @@ multiprocessing.set_start_method("spawn", force=True)
 
 # 날짜 설정
 # TODAY = datetime.now()
-TODAY = datetime(2025, 5, 9)
+TODAY = datetime(2025, 5, 10)
 YESTERDAY_DATE = TODAY - timedelta(days=1)
 TARGET_DATE = TODAY
 
