743fdd805b170e8971824e1ebbb406ee5da2877a
Quinsie <jyjh0501@naver.com>
2025-06-03 01:04:41 +0900

    [DEBUG]

--- backend/source/ai/buildSecondReviewParquet.py
+++ backend/source/ai/buildSecondReviewParquet.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+import time
 import argparse
 import pandas as pd
 from datetime import datetime, timedelta
@@ -13,6 +14,7 @@ REVIEW_JSON_DIR = os.path.join(BASE_DIR, "data", "preprocessed", "second_train",
 REVIEW_PARQUET_DIR = REVIEW_JSON_DIR  # 같은 경로에 저장
 
 def convert_json_to_parquet(target_date):
+    print("[INFO] Review Parquet 제작 시작...")
     json_path = os.path.join(REVIEW_JSON_DIR, f"{target_date}.json")
     parquet_path = os.path.join(REVIEW_PARQUET_DIR, f"{target_date}.parquet")
 
@@ -33,5 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("--date", required=True, help="대상 날짜 (YYYYmmDD 형식)")
     args = parser.parse_args()
 
+    now = time.time()
     target_date = datetime.strptime(args.date, "%Y%m%d") - timedelta(days=1)
     convert_json_to_parquet(datetime.strftime(target_date, "%Y%m%d"))
+    print("소요 시간: ", round(time.time() - now, 1), "sec")
